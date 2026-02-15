618354ccc63cc2edb11c2e5bead666e00e80e7f2
ashaile <ashaile27@gmail.com>
2026-02-15 10:03:16 +0900

    Test: Set environment variables in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import os
 import pytest
+
+# 테스트 실행 시 환경변수 강제 설정 (app import 전에 수행해야 함)
+os.environ['FLASK_ENV'] = 'development'
+os.environ['SECRET_KEY'] = 'test_secret_key'
+os.environ['ADMIN_PASSWORD'] = 'test_admin_password'
+
 from app import app, db
 from models import Application
 
